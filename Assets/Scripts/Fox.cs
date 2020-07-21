1ebe1be8633fd0a67fe8ce0952a8b2258fdb5043
kyeoreheo <91kyoheo@gmail.com>
2020-07-21 19:32:16 -0400

    very first commit

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    public Rigidbody2D myBody;
+    public Animator myAnimator;
+    public bool isOnFloor;
+
+    public float speed;
+    void Start()
+    {
+        Debug.Log("Hello :)");
+    }
+
+    void Update()
+    {
+        if (!isOnFloor)
+        {
+            myAnimator.Play("Player_Jump", 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+            if (isOnFloor)
+            {
+                myAnimator.Play("Player_Run", 0);
+            }
+        }
+        else if (Input.GetKey("right"))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            myBody.velocity = new Vector2(speed, myBody.velocity.y);
+            if (isOnFloor)
+            {
+                myAnimator.Play("Player_Run", 0);
+            }
+        }
+        else if(isOnFloor)
+        {
+            myAnimator.Play("Player_Idel", 0);
+        }
+
+        if (Input.GetKeyDown("space") && isOnFloor)
+        {
+            myBody.velocity = Vector2.up * speed * 2;
+            isOnFloor = false;
+        }
+
+        //var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        //transform.position += move * 1.0f * Time.deltaTime;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isOnFloor = true;
+       
+
+        }
+    }
+}
---
+++ Assets/Scripts/SecondLecture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondLecture : MonoBehaviour
+{ 
+    //This is how to create a value
+    //public [type] [name] = [value];
+    public int myInt = 0;
+    public string myString = "Hello";
+    //To make a string value, the value needs ""
+    public string cup = "water";
+
+    //public string bottle = 0;
+    //Value has to be matched with type.
+    public string number = "10";
+    //all the value starts with lower case, no spaces
+    //cup with milk -> cupWithMilk
+    //my int -> myInt, my string -> myString
+    //create a string, that named big cup, has coffee as a value
+    public string bigCup = "coffee";
+
+    //Challenge: IntroduceMe
+
+    public string myName = "Mike";
+
+    void Start()
+    {
+
+        //Debug.Log("name: " + myName + ", age: " + myAge + ", hasPet?: " + hasPet + ", petName: " + petName);
+    }
+
+    void Update()
+    {
+        
+    }
+}
