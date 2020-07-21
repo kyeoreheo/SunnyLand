3fb206cd8f4a60bd07c7e04c6c1db31e9f41ed55
kyeoreheo <91kyoheo@gmail.com>
2020-07-21 19:52:52 -0400

    camera movement and decorated game

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject fox;
+
+    void Update()
+    {
+        if (fox.transform.position.x >= -5)
+            this.transform.position = new Vector3(fox.transform.position.x, 0, -10);
+    }
+}
