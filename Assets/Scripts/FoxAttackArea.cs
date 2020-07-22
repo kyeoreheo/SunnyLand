d13f85f5189490618db82151127905c257d832b1
kyeoreheo <91kyoheo@gmail.com>
2020-07-21 21:58:31 -0400

    attached attackArea under the fox

---
+++ Assets/Scripts/FoxAttackArea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxAttackArea : MonoBehaviour
+{
+    public Fox fox;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            fox.Jump();
+            Destroy(col.gameObject);
+        }
+    }
+}
